5536d9d79daadb4efe646afafd953414a61d1257
Sergey Pankov <s.pankov@big3.ru>
2024-04-12 02:56:55 +0300

    debug

--- print_server/__main__.py
+++ print_server/__main__.py
@@ -10,8 +10,7 @@ import logging
 from dotenv import load_dotenv
 from pathlib import Path
 from os import getenv
-from pprint import pprint
-
+import tempfile
 
 log = logging.getLogger(__name__)
 
@@ -20,12 +19,15 @@ BASE_DIR = Path(__file__).parent
 load_dotenv()
 
 API_URL = TELEGRAM_API_TOKEN = getenv("API_URL", default="http://localhost:8000")
+REG_PRINT_SERVER_URL = f'{API_URL}/reg/'
+PRINT_JOB_URL_FORMAT = '{API_URL}/admin/printer/printjob/{task_id}'
 
-UPDATE_STATUS_URL = "http://example.com/api/update_status"  # URL для обновления статуса задачи
 PRINTER_NAME = "Your_Printer_Name"  # Можно задать конкретный принтер или выбрать из списка доступных
 CONNECTION_RETRY_TIMEOUT = 5
 FETCHING_JOBS_TIMEOUT = 5
-
+MAX_PRINT_WORKERS = 1
+DRY_RUN = False
+# DRY_RUN = True
 
 print_server_id = None
 printers_data = None
@@ -41,9 +43,8 @@ def get_mac(delimiter=':'):
     return delimiter.join(hex(b)[2:] for b in mac_int.to_bytes(6, byteorder='big'))
 
 
-async def reg_station(api_url):
+async def reg_station():
     global print_server_id, printers_data
-    url = f'{api_url}/reg/'
     mac_address = get_mac()
     printers_data = get_printers()
     log.info('Mac-address: %s', mac_address)
@@ -57,10 +58,9 @@ async def reg_station(api_url):
     async with aiohttp.ClientSession() as session:
         headers = {'Content-Type': 'application/json'}
         payload = json.dumps({'mac_address': mac_address, 'printers_data': printers_data,})
-        async with session.post(url, data=payload, headers=headers) as response:
-            response_text = await response.text()
+        async with session.post(REG_PRINT_SERVER_URL, data=payload, headers=headers) as response:
             log_func = log.info if response.status == 200 else log.error
-            log_func('CALL POST %s: %s', url, response.status)
+            log_func('CALL POST %s: %s', REG_PRINT_SERVER_URL, response.status)
             answer = await response.json()
             print_server_id = answer.get('print_server_id')
             return answer
@@ -73,26 +73,47 @@ async def fetch_print_tasks(session: ClientSession):
         return await response.json()
 
 
-async def update_task_status(session: ClientSession, task_id, status):
-    payload = {"task_id": task_id, "status": status}
-    async with session.post(UPDATE_STATUS_URL, json=payload) as response:
-        return await response.json()
-
-
-async def print_file(file_path, task_id, session: ClientSession):
+async def update_task_status(session: ClientSession, task_id, status, error_message=None):
+    payload = {"task_id": task_id, "status": status, 'error_message': error_message}
+    log.info('Update task #%s sratus to %s', task_id, status)
     try:
-        conn = cups.Connection()
-        conn.printFile(PRINTER_NAME, file_path, "Print Job", {})
-        await update_task_status(session, task_id, "printed")
+        async with session.post(f'{API_URL}/job/{task_id}/update', json=payload) as response:
+            return await response.json()
     except Exception as e:
-        await update_task_status(session, task_id, f"error: {str(e)}")
+        log.error("Can't update status of task #%s to %s.%s", task_id, status, f' ({error_message})')
 
 
-# Асинхронная обертка для функции печати
-async def async_print_file(file_path, task_id, session: ClientSession):
+async def print_file(job, session: ClientSession):
+    task_id = job['id']
+    file_url = job['file']['url']
+    file_name = job['file']['name']
+    printer_name = job['printer']
+    try:
+        await update_task_status(session, task_id, status="PRINTING")
+        with tempfile.NamedTemporaryFile(mode='w+b', suffix=f'__{file_name}') as fd:
+            async with session.get(file_url) as response:
+                while chunk := await response.content.read(1024):
+                    fd.write(chunk)
+            fd.seek(0)
+            conn = cups.Connection()
+            if DRY_RUN:
+                log.info(
+                    'PRINTING SUPPRESSED on %s: %s',
+                    printer_name, PRINT_JOB_URL_FORMAT.format(**{**locals(), **globals()}),
+                )
+            else:
+                conn.printFile(printer_name, fd.name, f"Print Job #{task_id}", {})
+    except Exception as e:
+        log.error('ERROR while job processing: %s', str(e))
+        await update_task_status(session, task_id, status="COMPLETED_WITH_ERROR", error_message=str(e))
+    else:
+        await update_task_status(session, task_id, status="COMPLETED_SUCCESSFULLY")
+
+
+async def async_print_file(job, session: ClientSession):
     loop = asyncio.get_event_loop()
-    with ThreadPoolExecutor() as pool:
-        await loop.run_in_executor(pool, print_file, file_path, task_id, session)
+    with ThreadPoolExecutor(max_workers=MAX_PRINT_WORKERS) as pool:
+        await loop.run_in_executor(pool, print_file, job, session)
 
 
 TASKS_QUEUE = {}
@@ -105,7 +126,7 @@ async def main():
     while True:
         try:
             log.info('Try to register on %s... %s', API_URL, try_number)
-            await reg_station(API_URL)
+            await reg_station()
         except ClientConnectorError as e:
             log.error('Connection errror: %s', e)
         except Exception as e:
@@ -134,10 +155,30 @@ async def main():
 
             while TASKS_QUEUE:
                 _, current_job = TASKS_QUEUE.popitem()
-                print(job)
+                log.debug(
+                    'Task #%(id)s (%(status)s) '
+                    'to print %(copies)s copies of '
+                    '%(file.name)r (%(file.url)s) '
+                    'on %(printer)r',
+                    flat_dict(job),
+                )
+                await print_file(job, session)
 
             await asyncio.sleep(FETCHING_JOBS_TIMEOUT)
-            # await async_print_file(task['file_path'], task['task_id'], session)
+
+
+def flat_dict(d_in, d_out=None, prefix=''):
+    d_out = d_out or {}
+    for k, v in d_in.items():
+        if isinstance(v, dict):
+            try:
+                flat_dict(v, d_out, prefix=f'{prefix}{k}.')
+            except:
+                pass
+            else:
+                continue
+        d_out[f'{prefix}{k}'] = v
+    return d_out
 
 
 if __name__ == '__main__':
