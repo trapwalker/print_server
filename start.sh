66ebb634e2fe852d486ab294e91e6586234b7218
Sergey Pankov <s.pankov@big3.ru>
2024-04-17 01:12:28 +0300

    start script fix

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
 
 cd /home/orangepi/print_server
-# kill here
-git pull
-pip3 install -r requirements.txt
-python3 -m print_server
+
+git pull > /home/orangepi/print_server/srv.log 2>&1
+pip3 install -r requirements.txt >> /home/orangepi/print_server/srv.log 2>&1
+python3 -m print_server >> /home/orangepi/print_server/srv.log 2>&1
