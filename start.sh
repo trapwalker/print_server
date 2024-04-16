4c64554e15c59ee76d400a25861d91f0fe039f9b
Sergey Pankov <s.pankov@big3.ru>
2024-04-17 01:53:28 +0300

    mute empty log messages

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
 p=/home/orangepi/print_server
-log=$p/srv.log
+log=/home/orangepi/srv.log
 cd $p
 echo "START! $(date) ----------------" > $log
+sleep 30
 git pull >> $log 2>&1
 pip3 install -r requirements.txt >> $log 2>&1
 python3 -m print_server >> $log 2>&1
